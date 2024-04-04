cad3e81d6aa17907b4fa24c89cb7023f0038a30b
Jonathan Boylan <jtylerboylan@outlook.com>
2024-04-04 15:33:57 -0400

    jetson

---
+++ run_big_brother_jetson.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+xhost +local:root
+
+# Get the absolute path to the directory containing this script
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Build the Docker image
+docker build -t big-brother-jetson:latest -f docker/Dockerfile.big-brother-jetson "${SCRIPT_DIR}"
+
+# Start the Docker container
+docker run -it --rm \
+    --privileged \
+    --runtime nvidia \
+    --network host \
+    --pid host \
+    --ipc host \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v "/${SCRIPT_DIR}:/app/src" \
+    big-brother-jetson:latest \
+    /bin/bash -c "source /opt/ros/humble/setup.bash && \
+                    colcon build --packages-select big_brother && \
+                    source /app/install/setup.bash && \
+                    ros2 launch big_brother big_brother_launch.py"
