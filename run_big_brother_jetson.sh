ab8650943d02ab673c2444eaf8c49f0bf707eb1a
Jonathan Boylan <jtylerboylan@outlook.com>
2024-04-04 16:20:34 -0400

    usb camera

--- run_big_brother_jetson.sh
+++ run_big_brother_jetson.sh
@@ -18,6 +18,14 @@ docker run -it --rm \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v "/${SCRIPT_DIR}:/app/src" \
+    --volume /tmp/argus_socket:/tmp/argus_socket \
+    --volume /etc/enctune.conf:/etc/enctune.conf \
+    --volume /etc/nv_tegra_release:/etc/nv_tegra_release \
+    --volume /tmp/nv_jetson_model:/tmp/nv_jetson_model \
+    --volume /var/run/dbus:/var/run/dbus \
+    --volume /var/run/avahi-daemon/socket:/var/run/avahi-daemon/socket \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    --volume $ROOT/data:/data \
     big-brother-jetson:latest \
     /bin/bash -c "source /opt/ros/humble/install/setup.bash && \
                     colcon build --packages-select big_brother && \
