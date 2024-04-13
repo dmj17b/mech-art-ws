4e5ddab32e7b76a33d5b0b03d863a95d6411a926
Jonathan Boylan <jtylerboylan@outlook.com>
2024-04-12 23:35:15 -0400

    jetson inference

--- run_ros_deep_learning.sh
+++ run_ros_deep_learning.sh
@@ -6,5 +6,5 @@ cd ${SCRIPT_DIR}/../jetson-inference && \
     ./docker/run.sh \
     --ros=humble \
     -v ${SCRIPT_DIR}:/app/src \
-    --run "source /opt/ros/humble/setup.bash && \
+    --run "./ros_entrypoint.sh && \
             ros2 launch /app/src/big_brother/launch/ros_deep_learning.launch"
