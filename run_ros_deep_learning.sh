0aed53a54b0c904c04340fae298823d90d5b04f9
Jonathan Boylan <jtylerboylan@outlook.com>
2024-04-12 22:29:10 -0400

    deep learning

--- run_ros_deep_learning.sh
+++ run_ros_deep_learning.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-cd ${SCRIPT_DIR}/../jetson-inference/docker && \
-    ./run.sh \
+cd ${SCRIPT_DIR}/../jetson-inference && \
+    ./docker/run.sh \
     --ros=humble \
     -v ${SCRIPT_DIR}:/app/src
