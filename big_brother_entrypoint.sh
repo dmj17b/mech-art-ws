1972e6ab28ceb83ab8353f2f67feaad1980f7ee6
Jonathan Boylan <jtylerboylan@outlook.com>
2024-04-12 23:55:40 -0400

    jetson inference

--- big_brother_entrypoint.sh
+++ big_brother_entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+source /opt/ros/humble/install/setup.bash
+source /ros_deep_learning/install/setup.bash
 ros2 launch /app/src/big_brother/launch/ros_deep_learning.launch
 
 exec "$@"
